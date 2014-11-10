dcb8d72866ed4e3a567dff25e41c3cb653753eaa
Malyshev Artem <proofit404@gmail.com>
2014-11-10 23:21:49 +0300

    Mention README in comments.

--- edbi-django.el
+++ edbi-django.el
@@ -22,23 +22,7 @@
 
 ;;; Commentary:
 
-;; To connect to databases defined in django settings module you must
-;; prepare your Emacs environment.  Two environment variables must have
-;; same values used to run django-admin.py command
-;;
-;;     M-x setenv RET DJANGO_SETTINGS_MODULE RET project.settings
-;;     M-x setenv RET PYTHONPATH RET /home/user/path/to/project/
-;;
-;; Also if you use virtual environment you need to provide
-;; `python-shell-virtualenv-path' variable
-;;
-;;     (setq python-shell-virtualenv-path "/home/user/path/to/env/")
-;;
-;; After Django project databases settings are available for you.  Run
-;; command below and select available databases with your preferred
-;; completing system
-;;
-;;     M-x edbi-django
+;; See the README for more details.
 
 ;;; Code:
 
