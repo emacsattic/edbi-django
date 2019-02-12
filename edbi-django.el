9b73db66c02a222fc62dc3bc590962f58c5b43d8
Artem Malyshev <proofit404@gmail.com>
2019-02-12 17:17:29 +0300

    Support PostGIS database adapter.

--- edbi-django.el
+++ edbi-django.el
@@ -42,6 +42,7 @@ print(dumps(settings.DATABASES))
 (defvar edbi-django-engines
   '(("django.db.backends.postgresql_psycopg2" . "Pg")
     ("django.db.backends.postgresql" . "Pg")
+    ("django.contrib.gis.db.backends.postgis" . "Pg")
     ("django.db.backends.sqlite3" . "SQLite")
     ("django.db.backends.oracle" . "Oracle")
     ("django.db.backends.mysql" . "mysql"))
